f9d8f91cff33eefd966b368976056c338639c301
jbarrfitz <jbarrowsfitzgerald@gmail.com>
2022-09-23 10:47:41 -0500

    added colors, fixed buttons, styled elements

--- src/App.tsx
+++ src/App.tsx
@@ -25,30 +25,47 @@ function App() {
       });
   };
 
+  const getColor = () => {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += Math.floor(Math.random() * 10);
+    }
+    return color;
+  };
+
+  const openInNewTab = (url: string) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <div className='App'>
-      <div id='quote-box'>
-        <p id='text'>
-          <i className='fa fa-quote-left'></i> {quoteData.quoteText}{' '}
-          <i className='fa fa-quote-right'></i>
-        </p>
-        <p id='author'>{quoteData.author}</p>
-        <button id='new-quote' onClick={getQuote}>
-          New Quote
-        </button>
-        <a
-          href={
-            'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-            quoteData.quoteText +
-            ' - ' +
-            quoteData.author
-          }
-          target='_blank'
-          rel='noreferrer'
-          id='tweet-quote'
-        >
-          Tweet
-        </a>
+      <h1>Random Quote Machine</h1>
+      <div className='container' style={{ backgroundColor: getColor() }}>
+        <div id='quote-box'>
+          <p id='text'>
+            <i className='fa fa-quote-left'></i> {quoteData.quoteText}{' '}
+            <i className='fa fa-quote-right'></i>
+          </p>
+          <p id='author'>{quoteData.author}</p>
+          <div className='actions'>
+            <button id='new-quote' onClick={getQuote}>
+              New Quote
+            </button>
+            <button
+              id='tweet'
+              onClick={() =>
+                openInNewTab(
+                  'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+                    quoteData.quoteText +
+                    ' - ' +
+                    quoteData.author
+                )
+              }
+            >
+              Tweet
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
